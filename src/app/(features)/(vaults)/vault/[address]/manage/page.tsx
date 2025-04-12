ee286a31e1f93b31ad2ff8367f6c0b0e446ee7c0
Tien Tran <tvtienit@gmail.com>
2025-04-13 06:12:58 +0700

    fix(vault manage): wrong async component

--- src/app/(features)/(vaults)/vault/[address]/manage/page.tsx
+++ src/app/(features)/(vaults)/vault/[address]/manage/page.tsx
@@ -3,7 +3,7 @@ import { NoSsrLayout } from '@/app-components/layout/NoSsrLayout';
 import VaultManage from '@/app-features/vaults/screens/VaultManage';
 import { useParams } from 'next/navigation';
 
-export default async function VaultManagePage() {
+export default function VaultManagePage() {
 	const { address } = useParams<{ address: string }>();
 	return (
 		<NoSsrLayout>
